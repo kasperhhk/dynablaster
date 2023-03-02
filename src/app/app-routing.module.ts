098441d7bb56db91145bb954d6eb784a9a7e1ba3
kasperhhk <kkr@dis-play.dk>
2023-03-02 11:16:04 +0100

    add game page + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamepageComponent } from './gamepage/gamepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: "game",
+    component: GamepageComponent
+}, {
+    pathMatch: "prefix",
+    path: "",
+    redirectTo: "/game"
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
